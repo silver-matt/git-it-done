e20ef0a0a852b312105ae927813435263eb29b95
Matt Silver <Mattsilver54@yahoo.com>
2022-08-06 13:55:32 -0700

    added form

---
+++ assets/js/homepage.js
@@ -0,0 +1,13 @@
+let getUserRepos = function(user) {
+    // format the github api url
+    let apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
+
+  getUserRepos();
